04fd27db868f78f9b7dac938e2c5f2b8fb8f5f56
Abijeet <abijeetpatro@gmail.com>
2021-01-26 13:23:36 +0530

    Remove unnecessary layout; change horizontal win % for Hexhat/Telegram
    Fix issue with Workspace 4 not moving to HDMI1

--- bin/display/activate_hdmi.sh
+++ bin/display/activate_hdmi.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # Activate HDMI
 xrandr --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
+## Sometimes workspace 4 refuses to go to HDMI1 when enabled, fix that
+i3-msg "workspace 4. ; move workspace to HDMI1"
